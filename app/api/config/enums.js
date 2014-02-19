fff4f8b7a84fa7738ed70b223a4f4da7ba0b72e6
Apo <jiyuny@yahoo.fr>
2014-02-19 17:53:27 +0100

    load infos before angular init

---
+++ app/api/config/enums.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Pluto on 12/30/13.
+ */
+var enums = enums || (function(){
+    var self = this;
+
+    self.elementTypeEnums = {
+        Text: 1,
+        Image: 2
+    };
+
+    return self;
+}())
